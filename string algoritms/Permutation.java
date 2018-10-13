65a420a8afa16364abd4ed35d0379b946c431f58
Anmol Kumari <32548472+anmolkumari@users.noreply.github.com>
2018-10-13 22:56:52 +0530

    Update Permutation.java
    solution using recursion with better complexity and lesser LOC

--- string algoritms/Permutation.java
+++ string algoritms/Permutation.java
@@ -13,47 +13,21 @@ public class SpecialStrings {
 
    public static void main(String[] args) {
   
-      String str = new String("abc");
-      System.out.println("the permutations of : " + str);
-      Stack temp=new Stack();
-      Stack perm=new Stack();
+       permuteit("aab","");
 
-      perm.push(str.charAt(str.length()-1));
-      int i,j;
-      for(i=str.length()-2;i>=0;i--){
-                char ch=str.charAt(i);
-                 System.out.println("play with : "+ch);
-                  System.out.println("perm"+perm);
-       while(perm.isEmpty()!=true){
+}
+    public static void permuteit(String str,String ans){
+        if(str.length()==0){
+                    System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+        String roq = str.substring(1);
+        for (int i = 0; i <= ans.length(); i++) {
+            String left = ans.substring(0, i);
+            String right = ans.substring(i);
+            permuteit(roq, right + ch + left);
 
-           String spp=perm.pop().toString();
-           StringBuilder s=new StringBuilder(spp);
-           int len=s.length();
-            System.out.println("s before : "+s);
-             StringBuilder stp=new StringBuilder(s);
-           for(j=0;j<len;j++){
-               s.insert(j,ch);
-                System.out.println("s  : "+s);
-                temp.push(s);
-                s=stp;
-
-           }
-          
-
-
-
-       }
-       while(temp.isEmpty()!=true){
-       perm.push(temp.pop());
-
-       }
-
-      }
-/*
-      System.out.println("perm"+perm);
-      System.out.println("temp"+temp);
-*/
-   
-   }
-
+        }
+    }
 }
