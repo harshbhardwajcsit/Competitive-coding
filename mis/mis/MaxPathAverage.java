f8ecc3e6de85b2fa609156f5e4ea2b5b2763a3cf
harshbhardwaj <harsh.bhardwaj@utopusinsights.com>
2021-05-04 20:13:30 +0530

    Problems updated

--- mis/mis/MaxPathAverage.java
+++ mis/mis/MaxPathAverage.java
@@ -45,6 +45,6 @@ public class MaxPathAverage {
 
     public static void main(String[] args) {
         Integer[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        System.out.println(getMaxAveragePath(matrix, 3));//5.2
+        System.out.println(getMaxAveragePath(matrix, 3));
     }
 }
